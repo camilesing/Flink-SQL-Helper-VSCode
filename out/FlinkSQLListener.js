e2bdba767788931fc4e6c80675b9196d6abf1a36
camilesing <camilesing@gmail.com>
2023-08-04 10:44:57 +0800

    remove out directory

--- out/FlinkSQLListener.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Generated from gen/FlinkSQL.g4 by ANTLR 4.9.0-SNAPSHOT
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=FlinkSQLListener.js.map
